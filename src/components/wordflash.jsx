46a80a96e492079d95cfc691e589cef636784f43
panjiajie <251907882@qq.com>
2020-07-12 21:29:53 +0800

    修改

---
+++ src/components/wordflash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./wordflash.less";
+export default class WordFlash extends React.Component {
+  render(){
+    // 虚拟dom
+    const temlate = 
+            <div className="wordflash">
+                ssss
+            </div>
+    return temlate;
+  }
+}
---
+++ src/pages/index/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WordFlash from "../../components/wordflash"
+function App() {
+  return (
+    <div className="App">
+      <WordFlash />
+    </div>
+  );
+}
+
+export default App;
